635048a6122fcbdab9dcb665119957c4f6412546
MaciejSzczesny <yetek.lol@gmail.com>
2021-01-06 17:17:42 +0100

    Added an implementation for test_get_weather_by_city_name_succesful

--- src/weather/weather.py
+++ src/weather/weather.py
@@ -1,3 +1,10 @@
+import requests
+
 class WeatherApp(object):
     def __init__(self):
-        pass
+        self.base_url = 'api.weatherapi.com/v1/'
+
+    def get_weather_by_city_name(self, city_name):
+        res = requests.get(self.base_url + city_name)
+        
+        return res

656b312b9df010d0cef98ef5273b4119f25a4a0c
MaciejSzczesny <yetek.lol@gmail.com>
2021-01-12 21:11:27 +0100

    Added test_add_calls_mongo_insert_one

--- src/weather_database/weather_database.py
+++ src/weather_database/weather_database.py
@@ -1,18 +1,6 @@
 from pymongo import MongoClient
 
 class WeatherDatabase(object):
-    def __init__(self, url):
-        self.client = MongoClient(url)
-        self.db = self.client.weather
-
-    def add(self, weather):
-        result = self.db.insert_one(weather)
-        return result
-
-    def find(self, city_name):
-        result = self.db.find({"city": city_name})
-        return result
-
-    def delete(self, city_name):
-        result = self.db.delete_one({"city": city_name})
-        return result
+    def __init__(self, url, mongo_client = MongoClient):
+        self.client = mongo_client(url)
+        self.db = self.client.db

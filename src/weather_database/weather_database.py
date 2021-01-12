ef9001f040199f35307a1a24009f8674b1c76bb4
MaciejSzczesny <yetek.lol@gmail.com>
2021-01-12 21:11:47 +0100

    Added an implementation for test_add_calls_mongo_insert_one

--- src/weather_database/weather_database.py
+++ src/weather_database/weather_database.py
@@ -4,3 +4,7 @@ class WeatherDatabase(object):
     def __init__(self, url, mongo_client = MongoClient):
         self.client = mongo_client(url)
         self.db = self.client.db
+
+    def add(self, weather):
+        result = self.db.insert_one(weather)
+        return result

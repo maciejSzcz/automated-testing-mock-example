acb0a3fd74bb5f62570c4b8549e3e50e2abafa36
MaciejSzczesny <yetek.lol@gmail.com>
2021-01-12 23:50:29 +0100

    Added an implementation for test_get_random_cities_weather_server_error_returns_none

--- src/weather/weather.py
+++ src/weather/weather.py
@@ -131,6 +131,9 @@ class WeatherApp(object):
     def get_random_cities_weather(self):
         res = requests.get(self.base_url + "random")
 
-        return res
+        if res.status != 200:
+            return None
+        else:
+            return res
 
     

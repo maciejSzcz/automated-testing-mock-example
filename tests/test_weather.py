57feddf7a6fe943d8fba1c542118e1ecede67f60
MaciejSzczesny <yetek.lol@gmail.com>
2021-01-12 21:03:10 +0100

    Added fake_database

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -3,11 +3,11 @@ from unittest.mock import Mock, patch, MagicMock
 import unittest
 
 from src.weather.weather import WeatherApp
-
+from .fake_database import FakeDatabase
 
 class TestWeatherApp(TestCase):
     def setUp(self):
-        fake_database = Mock()
+        fake_database = FakeDatabase()
         self.temp = WeatherApp(fake_database)
 
     @patch('src.weather.weather.requests.get')

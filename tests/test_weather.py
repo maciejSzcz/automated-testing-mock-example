9c2655386ead47f7098b10c27974d4e0b95ecfcb
MaciejSzczesny <yetek.lol@gmail.com>
2021-01-14 00:01:48 +0100

    Added test_delete_saved_weather_by_city_raises_error_with_city_not_in_db

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -17,6 +17,15 @@ class TestWeatherApp(TestCase):
 
         self.temp.weather_database.delete.assert_called_once_with("Warsaw")
 
+    def test_delete_saved_weather_by_city_raises_error_with_city_not_in_db(self):
+        self.temp.weather_database.delete = Mock()
+        self.temp.weather_database.delete.side_effect = ValueError("city not in database")
+
+        with self.assertRaisesRegex(ValueError, "city not in database"):
+            self.temp.delete_saved_weather_by_city("Warsaw")
+
+        
+
     @patch('src.weather.weather.requests.get')
     def test_while_save_weather_single_city_calls_save_weather_single_city(self, mock_get):
         self.temp.weather_database.add = Mock()
